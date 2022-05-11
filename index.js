f6ce479acdbf59032e7079845fc4fe005c36393b
fahimabushra1 <bushra.rifeen@gmail.com>
2022-05-11 14:08:27 +0600

    delete query

--- index.js
+++ index.js
@@ -31,7 +31,17 @@ async function run() {
             const product = await productCollection.findOne(query)
             res.send(product);
         })
+
+
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
+
     finally {
 
     }
