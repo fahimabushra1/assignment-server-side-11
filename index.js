aec23dee72c8913f2b1fd0bdccd625c47b1222a2
fahimabushra1 <bushra.rifeen@gmail.com>
2022-05-18 20:42:34 +0600

    heroku deployment

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.stdhw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -17,6 +34,7 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db('smartBike').collection('product');
+        const myItemsCollection = client.db('smartBike').collection('myitems');
 
         app.get('/product', async (req, res) => {
             const query = {}
@@ -35,9 +53,47 @@ async function run() {
 
         app.post('/product', async (req, res) => {
             const newProduct = req.body;
+            console.log(newProduct)
             const result = await productCollection.insertOne(newProduct);
             res.send(result);
         });
+
+
+
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const updatedQuantity = req.body
+            const filter = { _id: ObjectId(id) };
+            const option = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedQuantity.updatedQuantityCount
+                }
+            }
+            const result = await productCollection.updateOne(filter, updatedDoc, option);
+            res.send(result)
+            console.log(result)
+        })
+
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const deletedQuantity = req.body
+            console.log(deletedQuantity)
+            const filter = { _id: ObjectId(id) };
+            const option = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: deletedQuantity.deletedQuantityCount
+                }
+            }
+            const result = await productCollection.updateOne(filter, updatedDoc, option);
+            res.send(result)
+            console.log(result)
+        })
+
+
 
         app.delete('/product/:id', async (req, res) => {
             const id = req.params.id;
@@ -46,7 +102,28 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/myitems', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = myItemsCollection.find(query);
+                const addedItems = await cursor.toArray();
+                res.send(addedItems);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+        })
+
+        app.post('/myitems', async (req, res) => {
+            const items = req.body;
+            const result = await myItemsCollection.insertOne(items);
+            res.send(result);
+        })
+
     }
+
 
     finally {
 
